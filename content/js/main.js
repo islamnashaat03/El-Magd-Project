e2a9d76006aaf08f157440f2e10388b057fdfcaf
islamnashaat03 <islamnashaat03@gmail.com>
2021-06-20 16:56:42 +0200

    a

---
+++ content/js/main.js
@@ -0,0 +1,18 @@
+
+    var swiper = new Swiper(".mySwiper ", {
+    slidesPerView: 2,
+    spaceBetween: 50,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+    var swiper = new Swiper(".mySwiper ", {
+    slidesPerView: 2,
+    spaceBetween: 50,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
