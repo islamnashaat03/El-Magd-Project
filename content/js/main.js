f18313275367e23cd9635bf6f81e03e918a4eef1
islamnashaat03 <islamnashaat03@gmail.com>
2021-06-23 15:52:54 +0200

    ..

--- content/js/main.js
+++ content/js/main.js
@@ -6,13 +6,30 @@
       el: ".swiper-pagination",
       clickable: true,
     },
-  });
-    var swiper = new Swiper(".mySwiper ", {
-    slidesPerView: 2,
-    spaceBetween: 50,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
     },
   });
+  $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:50,
+    rtl:true,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:1,
+            nav:false
+        },
+        1000:{
+            items:1,
+            nav:true,
+            loop:false
+        }
+    }
+})
 
