b627a646c829d92578c46cc602e3918445cf0450
islamnashaat03 <islamnashaat03@gmail.com>
2021-06-26 11:13:49 +0200

    s

--- content/js/main.js
+++ content/js/main.js
@@ -1,6 +1,6 @@
 
     var swiper = new Swiper(".mySwiper ", {
-    slidesPerView: 2,
+    slidesPerView: 1,
     spaceBetween: 50,
     pagination: {
       el: ".swiper-pagination",
@@ -9,6 +9,20 @@
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+      640: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 1,
+        spaceBetween: 40,
+      },
+      1024: {
+        slidesPerView: 2,
+        spaceBetween: 50,
+      },
     },
   });
   $('.owl-carousel').owlCarousel({
