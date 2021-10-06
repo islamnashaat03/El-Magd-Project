3e56c18bcd1478a19b89e552c37edce669408ad7
islamnashaat03 <islamnashaat03@gmail.com>
2021-10-06 16:28:58 +0200

    edit our sevices page

--- content/js/main.js
+++ content/js/main.js
@@ -48,3 +48,23 @@ var swiper = new Swiper(".mySwiper ", {
     }
 });
 
+$(document).ready(function () {
+    // START SCROLL TO TOP BUTTON
+    if (window.matchMedia('(min-width:992px)').matches){
+      let span = document.querySelector(".up");
+      window.onscroll = () => {
+        if (scrollY >= 800) {
+          span.classList.add("active");
+        } else {
+          span.classList.remove("active");
+        }
+      };
+      span.onclick = () => {
+        window.scrollTo({
+          top:0,
+          behavior:"smooth",
+        })
+      }
+    }
+    // END SCROLL TO TOP BUTTON
+})
