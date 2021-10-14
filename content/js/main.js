d979508423c7c37dd651f8c7bbb749984c6123ef
islamnashaat03 <islamnashaat03@gmail.com>
2021-10-14 10:27:30 +0200

    edit hamburger icon

--- content/js/main.js
+++ content/js/main.js
@@ -68,3 +68,28 @@ $(document).ready(function () {
     }
     // END SCROLL TO TOP BUTTON
 })
+
+var animating = false;
+
+$('.hamburger-box').on('click touch', function() {
+  if(!animating) {
+    animating = true;
+    $(this).toggleClass('open');
+    $('.loop-glass-box').toggleClass('disable');
+    $('.cp').toggleClass('open open-menu');
+    setTimeout(function() {
+      animating = false;
+    }, 300);
+  }
+});
+$('.loop-glass-box').on('click touch', function() {
+  if(!animating) {
+    animating = true;
+    $(this).toggleClass('open');
+    $('.hamburger-box').toggleClass('disable');
+    $('.cp').toggleClass('open open-search');
+    setTimeout(function() {
+      animating = false;
+    }, 300);
+  }
+});
