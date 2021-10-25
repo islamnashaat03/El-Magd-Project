0935a3b78a4141e2d7a3e30aa8b4af8d000fac50
islamnashaat03 <islamnashaat03@gmail.com>
2021-10-25 10:05:23 +0200

    d

--- content/js/main.js
+++ content/js/main.js
@@ -75,21 +75,9 @@ $('.hamburger-box').on('click touch', function() {
   if(!animating) {
     animating = true;
     $(this).toggleClass('open');
-    $('.loop-glass-box').toggleClass('disable');
-    $('.cp').toggleClass('open open-menu');
     setTimeout(function() {
       animating = false;
     }, 300);
   }
 });
-$('.loop-glass-box').on('click touch', function() {
-  if(!animating) {
-    animating = true;
-    $(this).toggleClass('open');
-    $('.hamburger-box').toggleClass('disable');
-    $('.cp').toggleClass('open open-search');
-    setTimeout(function() {
-      animating = false;
-    }, 300);
-  }
-});
+
